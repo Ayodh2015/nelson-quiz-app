eef852d33b75d59a76157caa7e529722fe676760
Praneeth Jayasinghe <43755970+Ayodh2015@users.noreply.github.com>
2026-02-18 13:13:27 +0000

    Fix flask_login error and pin Python version

--- routes/auth.py
+++ routes/auth.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
-from flask_login import login_user, logout_user, login_required
 from config import supabase
 import bcrypt
 import uuid
